ea0793894307e9a12dbd94b99aa995baa942dd4c
Tanmoy Sarker <25175610+tanmoysarker@users.noreply.github.com>
2022-03-02 12:50:04 +0600

    Updates

---
+++ precache-manifest.38a8f8aab294d9e272d19795d1025f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d6ff9c10d60b3c9dae2",
+    "url": "/profile/static/css/main.142fac54.chunk.css"
+  },
+  {
+    "revision": "8d6ff9c10d60b3c9dae2",
+    "url": "/profile/static/js/main.8d6ff9c1.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.189fd871.js"
+  },
+  {
+    "revision": "2cc737e7a313b61de763",
+    "url": "/profile/static/js/2.2cc737e7.chunk.js"
+  },
+  {
+    "revision": "d3dc32ed6735b5d718bf0db3c27e5872",
+    "url": "/profile/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.7b29789b47cf5fb73c34fce134a01145.js"
+  "/profile/precache-manifest.38a8f8aab294d9e272d19795d1025f34.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/React-Portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/profile/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
