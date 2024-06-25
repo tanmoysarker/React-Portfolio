ba855b035c7a251acb169647c695c6d1687ad04b
Tanmoy Sarker <25175610+tanmoysarker@users.noreply.github.com>
2024-06-25 19:15:11 +1000

    testing

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -14,7 +14,7 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1>Hi! <br />I'm Tanmoy.</h1>
+                          <h1>Hi! <br />I'm Tanmoy</h1>
                           <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1iEkSpwmJaK987DvUQKU100onAOVNjEl9/view" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
